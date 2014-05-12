e808dea1feefbbf84e2e51cef4ebd376296e72f1
avanza <avanza@10.0.2.15>
2014-05-12 11:25:36 +0100

    Versión1 :)

---
+++ src/ejemplo/Ejemplo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author avanza
+ */
+public class Ejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic herepublic class operadorAsignacion {
+int i,j;
+double x;
+char c;
+boolean b;
+String s;
+i = 15;
+j = i;
+x = 12.345;
+c = 'A';
+b = false;
+s = "Hola";
+System.out.println("i = " + i);
+System.out.println("j = " + j);
+System.out.println("x = " + x);
+System.out.println("c = " + c);
+System.out.println("b = " + b);
+System.out.println("s = " + s);
+
+}
+    }
